6733032be0a670e063673c5f8caf55a7e2018d29
masa0922 <marcos.sanchez@netcraker.com>
2023-07-02 22:38:55 -0600

    Update of the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Cuadricula from './components/Cuadricula.jsx'
+import './main.css'
+import Pagina404 from './components/404.jsx'
+import App from './components/App.jsx'
+import Home from './Home.jsx'
+import CriptoPage from './components/cripto/CriptoPage.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} >
+          <Route index element={<Home />} />
+        </Route>
+        <Route path='/criptomonedas' element={<App />} >
+          <Route index element={<Cuadricula />} />
+          <Route path=':id' element={<CriptoPage />} />
+        </Route>
+        <Route path='*' element={<Pagina404 />} />
+      </Routes>
+    </BrowserRouter>
+)
